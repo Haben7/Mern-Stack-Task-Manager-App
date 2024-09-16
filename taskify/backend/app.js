bdcc56d6c7b27de824cf21ca80e99914d41c3f8d
Amnen7 <orthodoxawit7@gmail.com>
2024-09-16 14:21:33 +0300

    correct cors

--- taskify/backend/app.js
+++ taskify/backend/app.js
@@ -12,7 +12,7 @@ const authenticateToken = require('./routes/auth');
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'http://localhost:3000, https://mern-stack-task-manager-app-1.onrender.com',
   credentials: true,
 }));
 
