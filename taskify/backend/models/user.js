596457d7c686cca87f52e9ca617aa946d96c0250
Amnen7 <orthodoxawit7@gmail.com>
2024-09-18 23:04:14 +0300

    ,.

--- taskify/backend/models/user.js
+++ taskify/backend/models/user.js
@@ -10,7 +10,12 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    email:true,
+    validate: {
+      validator: function (v) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+      },
+      message: props => `${props.value} is not a valid email address!`
+    }
   },
   password: {
     type: String,
